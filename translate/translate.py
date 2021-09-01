ab74425be5015cd4cd22610aeca7f57cd8061407
Сергей Кумейко <kumeiko.se@gpu10.ipavlov.mipt.ru>
2021-09-01 15:43:38 +0300

    fix bag with first letter

--- translate/translate.py
+++ translate/translate.py
@@ -4,22 +4,35 @@ class translator():
     def __init__(self, name_model):
         self.tokenizer = FSMTTokenizer.from_pretrained(name_model)
         self.model = FSMTForConditionalGeneration.from_pretrained(name_model)
-    def translate_question(self, question):
+    def translate_phrase(self, question):
         input_ids = self.tokenizer.encode(question, return_tensors="pt")
         outputs = self.model.generate(input_ids)
         decode = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
         return decode
 
+def preproc_phrase(phrase):
+    if len(phrase) == 0:
+        return phrase
+    else:
+        return phrase[0].upper() + phrase[1:]
+
 def translate_batch(batch, ru_eng_translator, eng_ru_translator, ques_or_ans_key = "question"):
     for dict_ in batch:
         if dict_['lang'] == 'ru':
             if ques_or_ans_key == "question":
-                dict_[f"{ques_or_ans_key}_translate"] = eng_ru_translator.translate_question(dict_[ques_or_ans_key])
+                dict_[f"{ques_or_ans_key}_translate"] = eng_ru_translator.translate_phrase(
+                    preproc_phrase(dict_[ques_or_ans_key])
+                )
             elif ques_or_ans_key == "answer":
-                dict_[f"{ques_or_ans_key}_translate"] = ru_eng_translator.translate_question(dict_[ques_or_ans_key])
+                dict_[f"{ques_or_ans_key}_translate"] = ru_eng_translator.translate_phrase(
+                    preproc_phrase(dict_[ques_or_ans_key])
+                )
         elif dict_['lang'] == 'en':
             if ques_or_ans_key == "question":
-                dict_[f"{ques_or_ans_key}_translate"] = ru_eng_translator.translate_question(dict_[ques_or_ans_key])
+                dict_[f"{ques_or_ans_key}_translate"] = ru_eng_translator.translate_phrase(
+                    preproc_phrase(dict_[ques_or_ans_key])
+                )
             elif ques_or_ans_key == "answer":
-                dict_[f"{ques_or_ans_key}_translate"] = eng_ru_translator.translate_question(dict_[ques_or_ans_key])
-
+                dict_[f"{ques_or_ans_key}_translate"] = eng_ru_translator.translate_phrase(
+                    preproc_phrase(dict_[ques_or_ans_key])
+                )
